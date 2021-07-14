743394de778ce2060d4104e947f727290cfb2473
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-14 10:52:31 +0530

    Complete job post layout

--- src/containers/jobs.js
+++ src/containers/jobs.js
@@ -23,35 +23,37 @@ const Jobs = ({ fetchJobs, jobs, fetching }) => {
 
   if (!fetching) {
     toRender = jobs.results && jobs.results.map((i) => (
-      <Box key={i.id} maxW="full" borderWidth="1px" borderRadius="lg" m="8">
-        <Image
-          borderRadius="full"
-          boxSize="50px"
-          fit="cover"
-          src={i.organizations[0].picture}
-          alt="company logo"
-        />
-        <Box textAlign="left" pl="4">
-          <Box display="flex" justifyContent="space-between">
-            <Text fontSize="md">{i.objective}</Text>
+      <Box key={i.id} maxW="full" borderWidth="1px" borderRadius="lg" p="4" m="8">
+        <Box display="flex">
+          <Image
+            borderRadius="full"
+            boxSize="50px"
+            fit="cover"
+            src={i.organizations[0].picture}
+            alt="company logo"
+          />
+          <Box textAlign="left" pl="4">
+            <Text fontSize="md" pb="2">{i.objective}</Text>
+            <Text fontSize="sm">{i.organizations[0].name}</Text>
+            <Text fontSize="sm" color="whiteAlpha.700">
+              {`
+                ${i.remote ? 'Remote' : ''}
+                ${i.locations.length > 0 ? ` | ${i.locations[0]}` : ''}`}
+            </Text>
             <Text fontSize="sm">
+              Posted on
+              {' '}
               {new Date(i.created).toLocaleString('en-us', { day: 'numeric', month: 'long' })}
             </Text>
+            <Text fontSize="sm" color="green.500">
+              Salary:
+              {' '}
+              {`USD ${i.compensation.data.minHourlyUSD.toFixed(2)} to
+              USD ${i.compensation.data.maxHourlyUSD.toFixed(2)}`}
+              {' '}
+              hourly
+            </Text>
           </Box>
-          <Text fontSize="sm">{i.organizations[0].name}</Text>
-          <Text fontSize="sm" color="whiteAlpha.700">
-            {`
-              ${i.remote ? 'Remote' : ''}
-              ${i.locations.length > 0 ? ` | ${i.locations[0]}` : ''}`}
-          </Text>
-          <Text fontSize="sm" color="green.500">
-            Salary:
-            {' '}
-            {`USD ${i.compensation.data.minHourlyUSD.toFixed(2)} to
-            USD ${i.compensation.data.maxHourlyUSD.toFixed(2)}`}
-            {' '}
-            hourly
-          </Text>
         </Box>
       </Box>
     ));
