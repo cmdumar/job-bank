1d0a1288c1ee78844786c93405c897c5a737ed3c
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-14 06:55:52 +0530

    Implement jobs fetching function

--- src/redux/actions/jobs.js
+++ src/redux/actions/jobs.js
@@ -1,8 +1,25 @@
+import axios from 'axios';
+import setError from './error';
+import setFetching from './fetching';
 import types from './types';
 
 const setJobs = (jobs) => ({
-  jobs,
+  payload: jobs,
   type: types.SET_JOBS,
 });
 
-export default setJobs;
+const fetchJobs = (page) => async (dispatch) => {
+  try {
+    dispatch(setFetching(true));
+    const jobs = axios.post(`https://search.torre.co/opportunities/_search/?offset=${page}&size=15`, {});
+    if (jobs.data) {
+      dispatch(setJobs(jobs.data));
+      dispatch(setFetching(false));
+    }
+  } catch (error) {
+    dispatch(setError(error));
+    dispatch(setFetching(false));
+  }
+};
+
+export default fetchJobs;
