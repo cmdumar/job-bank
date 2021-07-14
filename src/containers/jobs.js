de05733a5e7bb24fe5fcddbd86dc1d4285919d85
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-14 09:40:54 +0530

    Design the card layout

--- src/containers/jobs.js
+++ src/containers/jobs.js
@@ -1,4 +1,6 @@
-import { Box, Container } from '@chakra-ui/react';
+import {
+  Box, Container, Image, Text,
+} from '@chakra-ui/react';
 import { object, bool, func } from 'prop-types';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
@@ -21,8 +23,23 @@ const Jobs = ({ fetchJobs, jobs, fetching }) => {
 
   if (!fetching) {
     toRender = jobs.results && jobs.results.map((i) => (
-      <Box key={i.id}>
-        <p>{i.objective}</p>
+      <Box key={i.id} display="flex" alignItems="flex-start" m="8">
+        <Image
+          borderRadius="full"
+          boxSize="50px"
+          fit="cover"
+          src={i.organizations[0].picture}
+          alt="company logo"
+        />
+        <Box textAlign="left" pl="4">
+          <Text fontSize="md">{i.objective}</Text>
+          <Text fontSize="sm">{i.organizations[0].name}</Text>
+          <Text fontSize="sm" color="whiteAlpha.700">
+            {`
+              ${i.remote ? 'Remote' : ''}
+              ${i.locations.length > 0 ? ` | ${i.locations[0]}` : ''}`}
+          </Text>
+        </Box>
       </Box>
     ));
   }
