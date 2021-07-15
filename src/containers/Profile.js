f522f7e2bbaff9920774fa36b3f82b544dc684a7
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-15 19:18:41 +0530

    Add content to profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -5,7 +5,7 @@ import {
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
-  Container, Box, Image, Text, Grid, Heading,
+  Container, Box, Image, Text, Grid, Heading, Tag,
 } from '@chakra-ui/react';
 import fetchProfile from '../redux/actions/profile';
 
@@ -29,8 +29,8 @@ const Profile = ({
   if (status === 'resolved' && profile?.person) {
     const {
       person: {
-        name, picture, professionalHeadline, location,
-      },
+        name, picture, professionalHeadline, location, summaryOfBio,
+      }, strengths, languages,
     } = profile;
 
     return (
@@ -45,8 +45,27 @@ const Profile = ({
           </Box>
           <Box>
             <Heading as="h2" size="lg" fontWeight="500" pb="4">{name}</Heading>
-            <Text>{professionalHeadline}</Text>
-            <Text>{location.name}</Text>
+            <Text pt="1">{professionalHeadline}</Text>
+            <Text pt="1">{location.name}</Text>
+            <Text>
+              Languages
+              {languages?.map((lan) => ` - ${lan.language.name} (${lan.fluency})`)}
+            </Text>
+            <Box py="2">
+              <Text>{summaryOfBio}</Text>
+            </Box>
+            <Box py="2">
+              {strengths?.map((str) => (
+                <Tag
+                  colorScheme="telegram"
+                  key={str.id}
+                  mr="1"
+                  mb="1"
+                >
+                  {str.name}
+                </Tag>
+              ))}
+            </Box>
           </Box>
         </Grid>
       </Container>
