39a7b953fabbd35a4324fac92b3d0279bb0c1ea9
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-15 08:07:59 +0530

    Complete the job page design

--- src/containers/job.js
+++ src/containers/job.js
@@ -37,8 +37,11 @@ const Job = ({
   if (status === 'resolved') {
     const {
       objective, organizations, place: { remote, location },
-      compensation: { currency, minAmount, maxAmount }, created,
-      languages, strengths,
+      compensation: {
+        currency, minAmount, maxAmount, periodicity,
+      }, created,
+      languages, strengths, serpTags: { description },
+      details, members,
     } = job;
 
     return (
@@ -68,6 +71,8 @@ const Job = ({
             <Text fontSize="sm" pl="2">
               Salary:
               {` ${currency} ${numberWithCommas(minAmount)} to ${numberWithCommas(maxAmount)}`}
+              {' '}
+              {periodicity}
             </Text>
           </Box>
           <Box display="flex" alignItems="center" pt="2" fontWeight="300">
@@ -97,77 +102,35 @@ const Job = ({
         <Box py="4">
           <Heading as="h3" size="sm" fontWeight="600">DESCRIPTION</Heading>
           <Text pt="2">
-            The Faulkner Automotive Group is looking for an enthusiastic,
-            self-motivated Lot Attendant to join our team! Faulkner is a place you
-            can establish a career and grow with the organization. We provide training
-            to all of our employees and offer continued growth opportunities for those
-            that have excellent talent, energy and ambition to succeed. We offer a top-tier
-            benefits package to all full-time employees, including Medical,
-            Dental, Vision, 401K plus company match, Employee Referral Bonuses and Paid Vacation.
+            {details?.filter((obj) => obj.code === 'organizations')[0].content}
           </Text>
         </Box>
         <Divider />
         <Box py="4">
           <Heading as="h3" size="sm" fontWeight="600">RESPONSIBILITIES</Heading>
-          <Text pt="2">
-            The Faulkner Automotive Group is looking for an enthusiastic,
-            self-motivated Lot Attendant to join our team! Faulkner is a place you
-            can establish a career and grow with the organization. We provide training
-            to all of our employees and offer continued growth opportunities for those
-            that have excellent talent, energy and ambition to succeed. We offer a top-tier
-            benefits package to all full-time employees, including Medical,
-            Dental, Vision, 401K plus company match, Employee Referral Bonuses and Paid Vacation.
-          </Text>
+          <Text
+            pt="2"
+            dangerouslySetInnerHTML={{ __html: details?.filter((obj) => obj.code === 'requirements')[0].content.split('.').join('<br />') }}
+          />
         </Box>
         <Divider />
         <Box py="4">
           <Heading as="h3" size="sm" fontWeight="600">YOU'LL BE WORKING WITH</Heading>
           <SimpleGrid minChildWidth="200px" spacing="20px" pt="3">
-            <Box bg="gray.300" color="black" p="8" pb="12" borderRadius="md" w="100%" textAlign="center">
-              <Image
-                borderRadius="full"
-                boxSize="100px"
-                mx="auto"
-                src="https://starrgate.s3.amazonaws.com/users/a60d2a68c3e0289b05fe83d82954e64e55e75921/profile_9bg1jDP.jpg"
-                alt="Segun Adebayo"
-                mb="4"
-              />
-              <Text fontSize="md" fontWeight="500">John Wick</Text>
-              <Text fontSize="sm">Web Developer</Text>
-            </Box>
-            <Box bg="lightblue" color="black" p="8" w="100%" textAlign="center">
-              <Image
-                src="https://starrgate.s3.amazonaws.com/users/ec7551dff10b04f9c59337114989f6f0cba20c95/profile_WWH0UvD.jpg"
-                borderRadius="full"
-                boxSize="100px"
-                mx="auto"
-                alt="profile photo"
-              />
-              <Text fontSize="md" fontWeight="500">John Wick</Text>
-              <Text fontSize="sm">Web Developer</Text>
-            </Box>
-            <Box bg="gray.300" color="black" p="8" w="100%" textAlign="center">
-              <Image
-                src="https://starrgate.s3.amazonaws.com/users/a60d2a68c3e0289b05fe83d82954e64e55e75921/profile_9bg1jDP.jpg"
-                borderRadius="full"
-                boxSize="100px"
-                mx="auto"
-                alt="profile photo"
-              />
-              <Text fontSize="md" fontWeight="500">John Wick</Text>
-              <Text fontSize="sm">Web Developer</Text>
-            </Box>
-            <Box bg="gray.300" color="black" p="8" w="100%" textAlign="center">
-              <Image
-                src="https://starrgate.s3.amazonaws.com/users/ec7551dff10b04f9c59337114989f6f0cba20c95/profile_WWH0UvD.jpg"
-                borderRadius="full"
-                boxSize="100px"
-                mx="auto"
-                alt="profile photo"
-              />
-              <Text fontSize="md" fontWeight="500">John Wick</Text>
-              <Text fontSize="sm">Web Developer</Text>
-            </Box>
+            {members?.map(({ id, person: { name, picture, professionalHeadline } }) => (
+              <Box key={id} bg="gray.100" color="black" p="8" pb="12" borderRadius="md" w="100%" textAlign="center">
+                <Image
+                  borderRadius="full"
+                  boxSize="100px"
+                  mx="auto"
+                  src={picture}
+                  alt="Segun Adebayo"
+                  mb="4"
+                />
+                <Text fontSize="md" fontWeight="500">{name}</Text>
+                <Text fontSize="sm">{professionalHeadline}</Text>
+              </Box>
+            ))}
           </SimpleGrid>
         </Box>
       </Container>
